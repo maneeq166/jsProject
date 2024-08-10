b24108cb483509ed4b339544aa28b1b0fb8dd281
maneeq166 <143007174+maneeq166@users.noreply.github.com>
2024-08-10 19:22:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const body= document.getElementsByTagName("body")[0]
-function set(name){
-  body.style.backgroundColor=name;
-}
-function random(){
-  const red = Math.round(Math.random()*255);
-  const green = Math.round(Math.random()*255);
-  const blue = Math.round(Math.random()*255);
+const input = document.getElementById("input")
 
-  const color = `rgb(${red},${green},${blue})`;
-  body.style.backgroundColor=color;
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+function check() {
+  const value = input.value
+  const reversed = reverseString(value)
+  alert(reversed === value ? "It's a palindrome" : "It's not a palindrome")
 }
